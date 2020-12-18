c2d4e7704b22c5d97075c31b7021204189a28d7b
anyageexy <anya.alexeeva2001@mail.ru>
2020-12-18 11:15:46 +0500

    1 commit

---
+++ OOP4_1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace OOP4_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Circle_Panel = new System.Windows.Forms.Panel();
+            this.Coord_label = new System.Windows.Forms.Label();
+            this.Clear_button = new System.Windows.Forms.Button();
+            this.Del_button = new System.Windows.Forms.Button();
+            this.СlearStorage_button = new System.Windows.Forms.Button();
+            this.ShowCircle_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Circle_Panel
+            // 
+            this.Circle_Panel.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.Circle_Panel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Circle_Panel.Location = new System.Drawing.Point(10, 8);
+            this.Circle_Panel.Margin = new System.Windows.Forms.Padding(2);
+            this.Circle_Panel.Name = "Circle_Panel";
+            this.Circle_Panel.Size = new System.Drawing.Size(824, 490);
+            this.Circle_Panel.TabIndex = 0;
+            this.Circle_Panel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Circle_Panel_MouseDown);
+            this.Circle_Panel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Circle_Panel_MouseMove);
+            // 
+            // Coord_label
+            // 
+            this.Coord_label.AutoSize = true;
+            this.Coord_label.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Bold);
+            this.Coord_label.Location = new System.Drawing.Point(891, 9);
+            this.Coord_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Coord_label.Name = "Coord_label";
+            this.Coord_label.Size = new System.Drawing.Size(55, 18);
+            this.Coord_label.TabIndex = 1;
+            this.Coord_label.Text = "label1";
+            // 
+            // Clear_button
+            // 
+            this.Clear_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Clear_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Clear_button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Clear_button.Font = new System.Drawing.Font("Bookman Old Style", 10F, System.Drawing.FontStyle.Bold);
+            this.Clear_button.ForeColor = System.Drawing.Color.Olive;
+            this.Clear_button.Location = new System.Drawing.Point(884, 190);
+            this.Clear_button.Margin = new System.Windows.Forms.Padding(2);
+            this.Clear_button.Name = "Clear_button";
+            this.Clear_button.Size = new System.Drawing.Size(216, 74);
+            this.Clear_button.TabIndex = 2;
+            this.Clear_button.Text = "Очистить панель";
+            this.Clear_button.UseVisualStyleBackColor = false;
+            this.Clear_button.Click += new System.EventHandler(this.Clear_button_Click);
+            // 
+            // Del_button
+            // 
+            this.Del_button.BackColor = System.Drawing.Color.DeepPink;
+            this.Del_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Del_button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Del_button.Font = new System.Drawing.Font("Bookman Old Style", 10F, System.Drawing.FontStyle.Bold);
+            this.Del_button.ForeColor = System.Drawing.Color.Maroon;
+            this.Del_button.Location = new System.Drawing.Point(884, 268);
+            this.Del_button.Margin = new System.Windows.Forms.Padding(2);
+            this.Del_button.Name = "Del_button";
+            this.Del_button.Size = new System.Drawing.Size(216, 74);
+            this.Del_button.TabIndex = 3;
+            this.Del_button.Text = "Удалить объекты";
+            this.Del_button.UseVisualStyleBackColor = false;
+            this.Del_button.Click += new System.EventHandler(this.Del_button_Click);
+            // 
+            // СlearStorage_button
+            // 
+            this.СlearStorage_button.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.СlearStorage_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.СlearStorage_button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.СlearStorage_button.Font = new System.Drawing.Font("Bookman Old Style", 10F, System.Drawing.FontStyle.Bold);
+            this.СlearStorage_button.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.СlearStorage_button.Location = new System.Drawing.Point(884, 346);
+            this.СlearStorage_button.Margin = new System.Windows.Forms.Padding(2);
+            this.СlearStorage_button.Name = "СlearStorage_button";
+            this.СlearStorage_button.Size = new System.Drawing.Size(216, 74);
+            this.СlearStorage_button.TabIndex = 4;
+            this.СlearStorage_button.Text = "Очистить хранилище";
+            this.СlearStorage_button.UseVisualStyleBackColor = false;
+            this.СlearStorage_button.Click += new System.EventHandler(this.СlearStorage_button_Click);
+            // 
+            // ShowCircle_button
+            // 
+            this.ShowCircle_button.BackColor = System.Drawing.Color.PaleGreen;
+            this.ShowCircle_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ShowCircle_button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ShowCircle_button.Font = new System.Drawing.Font("Bookman Old Style", 10F, System.Drawing.FontStyle.Bold);
+            this.ShowCircle_button.ForeColor = System.Drawing.Color.DarkGreen;
+            this.ShowCircle_button.Location = new System.Drawing.Point(884, 424);
+            this.ShowCircle_button.Margin = new System.Windows.Forms.Padding(2);
+            this.ShowCircle_button.Name = "ShowCircle_button";
+            this.ShowCircle_button.Size = new System.Drawing.Size(216, 74);
+            this.ShowCircle_button.TabIndex = 5;
+            this.ShowCircle_button.Text = "Показать объекты хранилища";
+            this.ShowCircle_button.UseVisualStyleBackColor = false;
+            this.ShowCircle_button.Click += new System.EventHandler(this.ShowCircle_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1207, 621);
+            this.Controls.Add(this.ShowCircle_button);
+            this.Controls.Add(this.СlearStorage_button);
+            this.Controls.Add(this.Del_button);
+            this.Controls.Add(this.Clear_button);
+            this.Controls.Add(this.Coord_label);
+            this.Controls.Add(this.Circle_Panel);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа №4";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Circle_Panel;
+        private System.Windows.Forms.Label Coord_label;
+        private System.Windows.Forms.Button Clear_button;
+        private System.Windows.Forms.Button Del_button;
+        private System.Windows.Forms.Button СlearStorage_button;
+        private System.Windows.Forms.Button ShowCircle_button;
+    }
+}
+
---
+++ OOP4_1/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP4_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Функция обработки получения координат XY события передвижения курсора по панели
+        private void Circle_Panel_MouseMove(object sender, MouseEventArgs e)
+        {
+            Coord_label.Text = "X: " + e.X + " Y: " + e.Y;
+        }
+
+        //Функция обработки события передвижения курсора по форме(очищение метки)
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Coord_label.Text = "";
+        }
+
+        //Инициализация необходимых переменных
+        static int amtCells = 1;
+        int CountElem = 0;
+        int item = 0;
+        static readonly Color DefaultColor = Color.White;
+        static readonly Color SelectedColor = Color.Red;
+        myStorage storage = new myStorage(amtCells);
+
+        //Обработчик события Click кнопки "Очистить панель".
+        private void Clear_button_Click(object sender, EventArgs e)
+        {
+            Circle_Panel.Refresh(); //перерисовка
+            for (int i = 0; i < amtCells; ++i)
+                if (!storage.Empty(i))
+                {
+                    storage.objects[i].Is_Drawn = false;
+                    storage.objects[i].color = DefaultColor;
+                }
+        }
+
+        //Обработчик события Click кнопки "Удалить объекты" (удаление выделенных объектов)
+        private void Del_button_Click(object sender, EventArgs e)
+        {
+            //Если объект существует и окрашен в цвет выбранных объектов,то происходит..
+            if (storage.OccupiedCells(amtCells) != 0)
+                for (int i = 0; i < amtCells; ++i)
+                    if (storage.Empty(i) == false && storage.objects[i].color == SelectedColor)
+                        storage.DeleteObj(ref i); //удаление объекта из хранилища
+            Clear_button_Click(sender, e); //Очищение панели
+            ShowCircle_button_Click(sender, e); //Показ объектов хранилища
+        }
+
+        //Обработчик события Click кнопки "Очистить хранилище" 
+        private void СlearStorage_button_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < amtCells; ++i) //Для всех объектов хранилища
+                storage.DeleteObj(ref i); //Удаление объекта из хранилища
+            CountElem = 0; //Обнуляем счетчик, отвечающий за количество объектов в хранилище
+        }
+
+        //Обработчик события Click кнопки "Показать объекты хранилища" 
+        private void ShowCircle_button_Click(object sender, EventArgs e)
+        {
+            //Сначала очищается панель
+            Circle_Panel.Refresh();
+
+            //Если хранилище не пустое, то..
+            if (storage.OccupiedCells(amtCells) != 0)
+                for (int i = 0; i < amtCells; ++i)
+                {
+                    DrawingCircles(ref storage, i); //рисуется окружность,
+                    if (!storage.Empty(i))
+                        storage.objects[i].Is_Drawn = true; //устанавливается флаг(объект отрисован)
+                }
+        }
+
+        //Функция отрисовки окружности на панели
+        private void DrawingCircles(ref myStorage storage, int CountElem)
+        {
+            //Если ячейка хранилища не пуста, то..
+            if (storage.objects[CountElem] != null)
+            {
+                //создаем ручку, отрисовываем окружность с указанными параметрами
+                Pen pen = new Pen(storage.objects[CountElem].color, 3);
+                Circle_Panel.CreateGraphics().DrawEllipse(pen, storage.objects[CountElem].x,
+                    storage.objects[CountElem].y, storage.objects[CountElem].R * 2, storage.objects[CountElem].R * 2);
+            }
+        }
+
+        //Функция, убирающая выделение объектов
+        private void SelectionRemove(ref myStorage storage)
+        {
+            for (int i = 0; i < amtCells; ++i)
+                //Если хранилище не пусто, то происходит..
+                if (!storage.Empty(i))
+                {
+                    storage.objects[i].color = DefaultColor; //установка стандартного цвета
+                    if (storage.objects[i].Is_Drawn == true)
+                        DrawingCircles(ref storage, i); //перерисовка окружности
+                }
+        }
+
+        //Функция, возвращающая значение индекса объекта в хранилище и сранвивает координаты
+        private int CheckCircle(ref myStorage storage, int Size, int x, int y)
+        {
+            if (storage.OccupiedCells(Size) != 0)
+            {
+                for (int i = 0; i < Size; ++i)
+                    if (!storage.Empty(i)) //Если хранилище не пусто
+                    {
+                        if (Math.Sqrt(Math.Pow((x - storage.objects[i].x), 2) + Math.Pow((y - storage.objects[i].y), 2)) <= storage.objects[i].R)
+                            return i;
+                    }
+            }
+            return -1;
+        }
+
+        //Функция обработки события нажатия курсора на панель
+        private void Circle_Panel_MouseDown(object sender, MouseEventArgs e)
+        {
+            //Создается объект класса CCircle
+            CCircle circle = new CCircle(e.X, e.Y, DefaultColor);
+
+            //Получение индекса объекта
+            int SellectedItem = CheckCircle(ref storage, amtCells, circle.x, circle.y);
+            if (SellectedItem != -1)
+            {
+                //Если нажат ctrl, выделяем несколько объектов
+                if (Control.ModifierKeys == Keys.Control)
+                {
+                    int x = e.X - circle.R;
+                    int y = e.Y - circle.R;
+                    for (int i = 0; i < amtCells; ++i)
+                        if (!storage.Empty(i))
+                        {
+                            //проверка условия
+                            if (Math.Sqrt(Math.Pow((x - storage.objects[i].x), 2) + Math.Pow((y - storage.objects[i].y), 2)) <= storage.objects[i].R)
+                            {
+                                storage.objects[i].color = SelectedColor; //установка цвета выделенного объекта
+                                DrawingCircles(ref storage, i); //перерисовка окружности
+                            }
+                        }
+                }
+                else
+                {
+                    //Иначе выделяем 1 объект и снимаем выделение у остальных объектов хранилища
+                    int x = e.X - circle.R;
+                    int y = e.Y - circle.R;
+                    SelectionRemove(ref storage);
+                    for (int i = 0; i < amtCells; ++i)
+                        if (!storage.Empty(i))
+                        {
+                            if (Math.Sqrt(Math.Pow((x - storage.objects[i].x), 2) + Math.Pow((y - storage.objects[i].y), 2)) <= storage.objects[i].R)
+                            {
+                                storage.objects[i].color = SelectedColor; //установка цвета выделенного объекта
+                                DrawingCircles(ref storage, i); //перерисовка окружности
+                                break;
+                            }
+                        }
+                    //Перерисовываем окружность
+                    storage.objects[SellectedItem].color = SelectedColor;
+                    DrawingCircles(ref storage, SellectedItem);
+                }
+                return;
+            }
+            //Добавляем окружность в хранилище
+            storage.AddObj(CountElem, ref circle, ref amtCells, ref item);
+
+            //Снимаем выделение всех объектов хранилища
+            SelectionRemove(ref storage);
+
+            //Устанавливаем цвет выделяемого объекта на новый добавленный
+            storage.objects[item].color = SelectedColor;
+
+            //Отрисовываем окружность
+            DrawingCircles(ref storage, item);
+
+            //Увеличиваем счетчик количества объектов хранилища
+            ++CountElem;
+        }
+
+
+        public class CCircle
+        {
+            public int R = 80; //Задаем постоянный радиус
+            public int x, y;
+            public Color color = DefaultColor; //Установка цвета по умолчанию
+            public bool Is_Drawn = true; //Проверка на отрисовку окружности на панели
+
+            //Конструктор по умолчанию
+            public CCircle()
+            {
+                x = 0;
+                y = 0;
+            }
+
+            //Конструктор с параметрами
+            public CCircle(int x, int y, Color color)
+            {
+                this.x = x - R;
+                this.y = y - R;
+                this.color = color;
+            }
+
+            //Деструктор
+            ~CCircle() { }
+        }
+
+        //Класс хранилища
+        class myStorage
+        {
+            public CCircle[] objects;
+
+            //Конструктор с параметром
+            public myStorage(int amt)
+            {
+                objects = new CCircle[amt];
+                for (int i = 0; i < amt; ++i)
+                    objects[i] = null;
+            }
+
+            //Функция для выделения мест хранилища
+            public void Allocation(int amt)
+            {
+                objects = new CCircle[amt];
+                for (int i = 0; i < amt; ++i)
+                    objects[i] = null;
+            }
+
+            //Функция добавления объекта в хранилище 
+            public void AddObj(int index, ref CCircle NewObj, ref int Count, ref int item)
+            {
+                myStorage NewStorage = new myStorage(Count + 1);
+                for (int i = 0; i < Count; ++i)
+                    NewStorage.objects[i] = objects[i];
+                for (int i = Count; i < (Count + 1) - 1; ++i)
+                {
+                    NewStorage.objects[i] = null;
+                }
+
+                Count = Count + 1;
+                Allocation(Count);
+                for (int i = 0; i < Count; ++i)
+                    objects[i] = NewStorage.objects[i];
+                objects[index] = NewObj;
+
+                item = index;
+            }
+
+            //Функция удаления объекта из хранилища
+            public void DeleteObj(ref int amtElem)
+            {
+                if (objects[amtElem] != null)
+                {
+                    objects[amtElem] = null;
+                    amtElem--;
+                }
+            }
+
+            //Функции проверки на занятость места в хранилище
+            public bool Empty(int CountElem)
+            {
+                if (objects[CountElem] == null)
+                    return true;
+                else return false;
+            }
+
+            //Функция, определяющая кол-во занятых мест в хранилище
+            public int OccupiedCells(int Size)
+            {
+                int amtOccupied = 0;
+                for (int i = 0; i < Size; ++i)
+                    if (!Empty(i))
+                        ++amtOccupied;
+                return amtOccupied;
+            }
+
+            //Деструктор
+            ~myStorage() { }
+        };
+    }
+}
